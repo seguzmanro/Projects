6637a12e58bd0435a9df64ac1fa7dfe05d834d81
seguzmanro <sebas32294@gmail.com>
2021-07-02 12:27:04 -0500

    Added a docsting stating the purpose of the program

--- Python_projs/seq_simulation.py
+++ Python_projs/seq_simulation.py
@@ -1,4 +1,10 @@
+'''
+This little program helps in generating random nucleotide sequences of arbitrary lenght.
+These sequences may be used to test other programs/pipelines.
+'''
 import random
+
+
 def simul_seq(lenght_of_the_sequence):
     brute_res = []
     for i in range(lenght_of_the_sequence):
